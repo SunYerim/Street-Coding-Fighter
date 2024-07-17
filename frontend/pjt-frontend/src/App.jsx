573b07dd66a749dd0320e92a3b0956a5b768e505
dltkdgus482 <dltkdgus482@naver.com>
2024-07-17 16:42:26 +0900

    Init: frontend project files
    - 프론트엔드 초기 파일 생성
    - 커밋 템플릿 추가

---
+++ frontend/pjt-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Background from "./Background";
+
+function App() {
+  return (
+    <>
+      <Background />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/pjt-frontend/src/Background.jsx
@@ -0,0 +1,26 @@
+export default function Background() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      <img
+        src="/background.svg"
+        alt="background"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: 0,
+        }}
+      />
+    </div>
+  );
+}
