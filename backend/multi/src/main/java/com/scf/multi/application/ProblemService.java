50a80c7672ede46bd8a4c01c25a85c3c83afb5ea
김민욱 <kmw9093@gmail.com>
2024-07-19 10:49:32 +0900

    feat #1 : 문제 서비스에서 문제 가져오는 기능 개발

--- backend/multi/src/main/java/com/scf/multi/application/ProblemService.java
+++ backend/multi/src/main/java/com/scf/multi/application/ProblemService.java
@@ -1,8 +1,35 @@
 package com.scf.multi.application;
 
+import com.scf.multi.domain.dto.Problem;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class ProblemService {
 
+    private final WebClient webClient;
+
+    public ProblemService(WebClient.Builder webClientBuilder,
+        @Value("${problem.server.url}") String problemServerUrl) {
+        this.webClient = webClientBuilder.baseUrl(problemServerUrl).build();
+    }
+
+    public List<Problem> getProblems() {
+
+        // 예시 문제 반환 코드
+        List<Problem> problems = new ArrayList<>();
+        problems.add(new Problem(1, 1, 0, "정렬", "버블 정렬", "버블 정렬 내용", "{1:2, 2:3, 3:4, 4:4}"));
+        problems.add(new Problem(2, 2, 0, "정렬", "삽입 정렬", "삽입 정렬 내용", "{1:2, 2:3, 3:4, 4:4}"));
+        return problems;
+
+//        return webClient.get()
+//            .uri("/api/problems") // 나중에 협의 후 수정
+//            .retrieve()
+//            .bodyToFlux(Problem.class)
+//            .collectList()
+//            .block();
+    }
 }
