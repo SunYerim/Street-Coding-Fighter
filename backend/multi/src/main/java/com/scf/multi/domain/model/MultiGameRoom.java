bcaee2ce6c636008a1f7ee100e79b1dc4ea4c4a4
김민욱 <kmw9093@gmail.com>
2024-07-19 17:07:21 +0900

    feat #1 : 점수 및 랭킹 계산 기능 개발

--- backend/multi/src/main/java/com/scf/multi/domain/model/MultiGameRoom.java
+++ backend/multi/src/main/java/com/scf/multi/domain/model/MultiGameRoom.java
@@ -2,9 +2,13 @@ package com.scf.multi.domain.model;
 
 import com.scf.multi.domain.dto.Player;
 import com.scf.multi.domain.dto.Problem;
+import com.scf.multi.domain.dto.Rank;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,9 +18,11 @@ public class MultiGameRoom {
 
     private final List<Problem> problems = new ArrayList<>();
     private final List<Player> players = Collections.synchronizedList(new ArrayList<>());
+    private final Map<Long, Integer> scoreBoard = Collections.synchronizedMap(new HashMap<>());
     private String roomId;
     private Long hostId;
     private Boolean isStart;
+    private Integer round;
     
     public List<Player> getPlayers() {
         // 읽기 전용 List 반환
@@ -39,5 +45,33 @@ public class MultiGameRoom {
     public void gameStart(List<Problem> problems) {
         this.problems.addAll(problems);
         this.isStart = true;
+    }
+
+    public void updateScore(Long userId, int score) {
+
+        int newScore = scoreBoard.get(userId) + score;
+        this.scoreBoard.put(userId, newScore);
+    }
+
+    public void nextRound() {
+        this.round += 1;
+    }
+
+    public List<Rank> calculateRank() {
+        // scoreBoard의 userId와 score를 Rank 객체로 변환
+        List<Rank> ranks = scoreBoard.entrySet().stream()
+            .map(entry -> Rank.builder()
+                .userId(entry.getKey())
+                .username(players.stream()
+                    .filter(player -> player.getUserId().equals(entry.getKey()))
+                    .map(Player::getUsername)
+                    .findFirst()
+                    .orElse("Unknown"))
+                .score(entry.getValue())
+                .build())
+            .sorted((rank1, rank2) -> Integer.compare(rank2.getScore(), rank1.getScore()))
+            .collect(Collectors.toList());
+
+        return ranks;
     }
 }
