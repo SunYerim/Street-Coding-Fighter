19b89b8f1fad9c9dccdaa20550d424b4a326acc3
김민욱 <kmw9093@gmail.com>
2024-07-19 10:48:59 +0900

    feat #1 : problem 클래스 설계

--- backend/multi/src/main/java/com/scf/multi/domain/dto/Problem.java
+++ backend/multi/src/main/java/com/scf/multi/domain/dto/Problem.java
@@ -1,8 +1,17 @@
 package com.scf.multi.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Problem {
 
+    private Integer problemInfoId;
+    private Integer problemId;
+    private Integer type;
+    private String category;
+    private String title;
+    private String content;
+    private String answer;
 }
