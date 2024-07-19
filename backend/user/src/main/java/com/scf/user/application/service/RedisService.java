decefaa2ca3c47505bbc220e46ec8eaaf38b5a9c
선예림 <syl1602@naver.com>
2024-07-19 15:25:54 +0900

    feat: #2 RedisService 구현

---
+++ backend/user/src/main/java/com/scf/user/application/service/RedisService.java
@@ -0,0 +1,12 @@
+package com.scf.user.application.service;
+
+import java.time.Duration;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RedisService {
+    void setValues(String key, String value);
+    void setValues(String key, String value, Duration duration);
+    String getValue(String key);
+    void deleteValue(String key);
+}
