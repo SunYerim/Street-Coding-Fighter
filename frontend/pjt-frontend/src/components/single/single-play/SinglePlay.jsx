d563ed60b5d3ec780fc7d84e8a13d2f5a6f76865
여대기 <daegi0923@naver.com>
2024-07-19 12:45:01 +0900

    feat : single play-dynamic content

--- frontend/pjt-frontend/src/components/single/single-play/SinglePlay.jsx
+++ frontend/pjt-frontend/src/components/single/single-play/SinglePlay.jsx
@@ -1,28 +1,106 @@
-import './SinglePlay.css'
+import styled from 'styled-components';
 
+const PlayView = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('/src/assets/background-single.png');
+  background-size: cover;
+  color: black;
+`;
+
+const ImageBox = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100%;
+`;
+
+const DialogueBox = styled.div`
+  position: fixed;
+  bottom: 10px;
+  left: 5vw;
+  width: 90vw;
+  height: 180px;
+  background-color: rgba(240, 240, 240, 0.9);
+  border-radius: 0.4em;
+  font-size: 1.3em;
+
+  &:after {
+    content: '';
+    position : absolute;
+    left: 0;
+    top: 85%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-right-color: rgba(240, 240, 240, 0.8);
+    border-left: 0;
+    border-top: 0;
+    margin-top: -10px;
+    margin-left: -20px;
+  }
+`;
+
+const DialogueHeader = styled.div`
+  // display: flex;
+  justify-content: space-between;
+`;
+
+const DialogueButton = styled.button`
+  /* 버튼 스타일을 여기에 추가하세요 */
+`;
+const DialogueBody = styled.div`
+  /* 버튼 스타일을 여기에 추가하세요 */
+`;
+
+const CharacterImage = styled.img`
+  position: fixed;
+  height: 600px;
+  left: 30vw;
+  bottom: 0px;
+`;
+
+const CharacterName = styled.div`
+  position: absolute;
+  top: -10px;
+  left : 20px;
+  height : 20px;
+  font-size :1em;
+  border-radius: 10px;
+  background-color: rgba(240, 240, 240, 0.8);
+`;
 
 export default function SinglePlay() {
-  return (
-    <div id='play-view'>
-      <div id='image-box'> {/* image, fixed로 할 예정 */}
-        <img className = "character" src="/src/assets/character-test-removebg-preview.png" alt="" />
+  const dialogueList = [
+    {
+      page_no: 0,
+      script_content: '<p>첫번째줄 대사입니다.</p><p>두번째줄 대사입니다.</p><p>세번째줄 대사입니다.</p>'
+    },
+    {
+      page_no: 1,
+      script_content: '<p>첫번째줄 대사입니다.</p><p>두번째줄 대사입니다.</p><p>세번째줄 대사입니다.</p>'
+    },
+  ]
+  // const onClick = ()=>{
 
-      </div>
-      <div id='dialogue-box'>
-        {' '}
-        {/* dialogue box */}
-        <div id='dialogue-header'>
-          {' '}
-          {/* dialogue header */}
-          <button className='dialogue-btn'>이전</button>
-          <button className='dialogue-btn'>다음</button>
-        </div>
-        <div>
-          {' '}
-          {/* dialogue body */}
-          <p>대사 들어갈 자리 어쩌고 저쩌고</p>
-        </div>
-      </div>
-    </div>
+
+  // }
+  return (
+    <PlayView>
+      <ImageBox>
+        <CharacterImage src="/src/assets/character-test-removebg-preview.png" alt="" />
+      </ImageBox>
+      <DialogueBox >
+        <DialogueHeader>
+          <CharacterName>Hoshino Ai</CharacterName>
+        </DialogueHeader>
+        <DialogueBody dangerouslySetInnerHTML={{__html:dialogueList[0].script_content}}>
+        </DialogueBody>
+      </DialogueBox>
+    </PlayView>
   );
 }
