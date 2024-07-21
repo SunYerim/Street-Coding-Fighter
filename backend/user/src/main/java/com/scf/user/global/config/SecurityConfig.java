6b8e8050ec6a7b246512ecee54495012c7b621df
선예림 <syl1602@naver.com>
2024-07-22 00:18:07 +0900

    feat: #2 SecurityConfig.java 작성

--- backend/user/src/main/java/com/scf/user/global/config/SecurityConfig.java
+++ backend/user/src/main/java/com/scf/user/global/config/SecurityConfig.java
@@ -1,15 +1,86 @@
 package com.scf.user.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scf.user.application.service.RedisService;
+import com.scf.user.global.JwtAuthenticationFilter;
 import com.scf.user.global.JwtTokenProvider;
+import com.scf.user.global.LoginFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RedisService redisService;
 
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationConfiguration.class).getAuthenticationManager();
+    }
+
+    // 다양한 알고리즘을 사용가능. vs. BCryptPasswordEncoder -> 단일 알고리즘(BCrypt)만 사용.
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpsecurity) throws Exception {
+        httpsecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login", "/", "/join", "/reissue").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+            .addFilterAt(new LoginFilter(authenticationManager(httpsecurity), jwtTokenProvider, objectMapper(), redisService), UsernamePasswordAuthenticationFilter.class)
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpsecurity.build();
+
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
 
 
 }
