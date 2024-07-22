45e109343fd4dacb3d8f358a663bce9f4e65f1cb
박지훈 <zlxlgus123@naver.com>
2024-07-22 17:49:33 +0900

    feat #8 : battleGameController 생성

---
+++ backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
@@ -0,0 +1,26 @@
+package com.scf.battle.presentation.controller;
+
+import com.scf.battle.application.BattleGameService;
+import com.scf.battle.domain.model.BattleGameRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/battle")
+public class BattleGameController {
+
+    private final BattleGameService battleGameService;
+
+    @GetMapping("/room")
+    public ResponseEntity<?> roomList() {
+        //List<BattleGameRoom> rooms = battleGameService.findAllRooms();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
