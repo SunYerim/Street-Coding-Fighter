f2d7529cd1fe9dc6e28a855185272d2e7be4f81f
정범수 <jbss0708@gmail.com>
2024-07-22 16:30:33 +0900

    Design: Multi Create

--- frontend/pjt-frontend/src/components/multi/MultiCreate.jsx
+++ frontend/pjt-frontend/src/components/multi/MultiCreate.jsx
@@ -1,4 +1,5 @@
 import "../../index.css";
+import "../../css/GameMain.css";
 import Button from "./Button.jsx";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -9,7 +10,22 @@ export default function MultiCreate() {
   return (
     <>
       <div className="container">
-        Multi createRoom
+        <div className="create-container">
+          <h1>Create Room [ Multi Mode ]</h1>
+          <div className="pink-line"></div>
+          <div className="create-box">
+            <span>방 제목 : </span>
+            <input className="create-title" type="text" placeholder="Enter Room Title" maxLength={30} />
+          </div>
+          <div className="create-box">
+            <span>최대인원 : </span>
+            <input className="create-max-number" type="number" min="1" max="30" />
+          </div>
+          <div className="create-button-container">
+            <button className="create-button">Create</button>
+            <button className="create-button" onClick={() => navigate("/multi")}>Cancle</button>
+          </div>
+        </div>
       </div>
     </>
   );
