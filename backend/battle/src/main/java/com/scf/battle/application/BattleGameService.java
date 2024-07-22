c114ba20d48a7b53612008171c45e895d6d59964
박지훈 <zlxlgus123@naver.com>
2024-07-22 17:50:09 +0900

    feat #8 : battleGameService 생성

---
+++ backend/battle/src/main/java/com/scf/battle/application/BattleGameService.java
@@ -0,0 +1,9 @@
+package com.scf.battle.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BattleGameService {
+}
