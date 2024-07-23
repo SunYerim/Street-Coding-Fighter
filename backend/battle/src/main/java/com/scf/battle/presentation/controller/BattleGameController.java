a5a6359f0fc31ebbb486f953fc53342273adc004
박지훈 <zlxlgus123@naver.com>
2024-07-23 11:20:07 +0900

    feat #8 : BattleGameController roomList 작성

--- backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
+++ backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
@@ -1,11 +1,14 @@
 package com.scf.battle.presentation.controller;
 
 import com.scf.battle.application.BattleGameService;
+import com.scf.battle.domain.dto.JoinRoomDto;
 import com.scf.battle.domain.model.BattleGameRoom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +23,8 @@ public class BattleGameController {
 
     @GetMapping("/room")
     public ResponseEntity<?> roomList() {
-        //List<BattleGameRoom> rooms = battleGameService.findAllRooms();
-        return new ResponseEntity<>(HttpStatus.OK);
+        List<BattleGameRoom> rooms = battleGameService.findAllRooms();
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
     }
+
 }
