08dd2a3dbe4b7ca356f59c9f74aceaaea2b0658f
박지훈 <zlxlgus123@naver.com>
2024-07-23 11:21:03 +0900

    fix #8 : BattleGameController joinRoom post 수정

--- backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
+++ backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,9 +34,11 @@ public class BattleGameController {
         return new ResponseEntity<>(battleGameRoom, HttpStatus.OK);
     }
 
-    @GetMapping("/room/{roomId}")
+    @PostMapping("/room/{roomId}")
     public ResponseEntity<?> joinRoom(PathVariable roomId, @RequestBody JoinRoomDto joinRoomDto) {
         battleGameService.joinRoom(roomId, joinRoomDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @
 }
