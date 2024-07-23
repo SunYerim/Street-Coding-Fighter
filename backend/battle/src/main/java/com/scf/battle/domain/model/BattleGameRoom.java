8f257a81b2dddfba450d3956b37aa0591b694165
박지훈 <zlxlgus123@naver.com>
2024-07-23 12:48:44 +0900

    feat #8 : BattleGameRoom add 작성

--- backend/battle/src/main/java/com/scf/battle/domain/model/BattleGameRoom.java
+++ backend/battle/src/main/java/com/scf/battle/domain/model/BattleGameRoom.java
@@ -1,16 +1,20 @@
 package com.scf.battle.domain.model;
 
+import com.scf.battle.domain.dto.JoinRoomDTO;
 import com.scf.battle.domain.dto.Player;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.beans.factory.parsing.Problem;
 
 import java.util.*;
 
+@Getter
+@Builder
 public class BattleGameRoom {
     private String roomId;
     private Long hostId;
     private String title;
     private String password;
-    private Integer maxPlayer;
 
     private final List<Problem> problems = new ArrayList<>();
     private final List<Player> players = Collections.synchronizedList(new ArrayList<>());
@@ -21,4 +25,18 @@ public class BattleGameRoom {
     private List<Player> getPlayers(){
         return Collections.unmodifiableList(players);
     }
+
+    public void add(JoinRoomDTO joinRoomDto) {
+        Long userId = joinRoomDto.getUserId();
+        String username = joinRoomDto.getUsername();
+        String roomPassword = joinRoomDto.getRoomPassword();
+
+        if(roomPassword.equals(this.password)){
+            players.add(new Player(userId, username));
+        }
+        else{
+            // 예외처리
+        }
+    }
+
 }
