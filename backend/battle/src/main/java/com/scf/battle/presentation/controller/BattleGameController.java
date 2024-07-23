e663f6a2cc9fe103a3ab16716c661f18828397d6
박지훈 <zlxlgus123@naver.com>
2024-07-23 11:23:25 +0900

    feat #8 : BattleGameController createRoom 작성

--- backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
+++ backend/battle/src/main/java/com/scf/battle/presentation/controller/BattleGameController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +41,9 @@ public class BattleGameController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @
+    @PostMapping("/room")
+    public ResponseEntity<?> createRoom(@RequestHeader Long userId){
+        battleGameService.createRoom(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
