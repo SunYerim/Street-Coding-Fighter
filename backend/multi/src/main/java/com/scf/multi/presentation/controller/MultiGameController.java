ce19c20df678e60c378ca1391afd0f47ab463f3f
김민욱 <kmw9093@gmail.com>
2024-07-23 11:17:56 +0900

    fix #11 : 유저 정보를 Header로 받는 것으로 수정

--- backend/multi/src/main/java/com/scf/multi/presentation/controller/MultiGameController.java
+++ backend/multi/src/main/java/com/scf/multi/presentation/controller/MultiGameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,8 +44,8 @@ public class MultiGameController {
         return new ResponseEntity<>(players, HttpStatus.OK);
     }
 
-    @PostMapping("/room/{userId}")
-    public ResponseEntity<?> createRoom(@PathVariable Long userId) {
+    @PostMapping("/room")
+    public ResponseEntity<?> createRoom(@RequestHeader Long userId) {
         String roomId = multiGameService.createRoom(userId);
         return new ResponseEntity<>(roomId, HttpStatus.OK);
     }
