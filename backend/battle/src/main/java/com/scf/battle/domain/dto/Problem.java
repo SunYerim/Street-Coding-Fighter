da521975cc45a48c8a31b4401c20b1c68d1466d5
박지훈 <zlxlgus123@naver.com>
2024-07-23 15:47:05 +0900

    feat #8 : Prolbem 생성

---
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/Problem.java
@@ -0,0 +1,16 @@
+package com.scf.battle.domain.dto;
+
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Problem {
+    private Long problemId;
+    private Integer type;
+    private String category;
+    private String title;
+    private String content;
+    private Map<Integer, Integer> answer;
+}
