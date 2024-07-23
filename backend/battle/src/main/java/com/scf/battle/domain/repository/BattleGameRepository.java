3f62400cc631277bac40bdc99816535459ea0578
박지훈 <zlxlgus123@naver.com>
2024-07-23 11:39:49 +0900

    feat #8 : BattleGameRepository findAllRooms 작성

--- backend/battle/src/main/java/com/scf/battle/domain/repository/BattleGameRepository.java
+++ backend/battle/src/main/java/com/scf/battle/domain/repository/BattleGameRepository.java
@@ -1,4 +1,18 @@
 package com.scf.battle.domain.repository;
 
+import com.scf.battle.domain.model.BattleGameRoom;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BattleGameRepository {
+
+    private final Map<String, BattleGameRoom> gameRooms = Collections.synchronizedMap(new HashMap<>());
+
+    public List<BattleGameRoom> findAllRooms() {
+        return gameRooms.values().stream().toList();
+    }
 }
