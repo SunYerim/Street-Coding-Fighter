2ad9b341128633875bbfb0263621a0567fd37c17
박지훈 <zlxlgus123@naver.com>
2024-07-24 10:38:41 +0900

    feat #15 : battleRoom startGame시 문제를 가져와서 3개씩 나누어 담기

--- backend/battle/src/main/java/com/scf/battle/domain/model/BattleGameRoom.java
+++ backend/battle/src/main/java/com/scf/battle/domain/model/BattleGameRoom.java
@@ -38,16 +38,24 @@ public class BattleGameRoom {
     }
 
     public void startGame(List<Problem> problems, Long userId) {
-        if(!this.hostId.equals(userId)){ //방장이 아닐 때
+        if (!this.hostId.equals(userId)) { //방장이 아닐 때
             //예외처리
         }
-        if(this.players.size() < 2){ // 2명보다 작을 때
+        if (this.players.size() < 2) { // 2명보다 작을 때
             //예외처리
         }
-        if (problems == null || problems.isEmpty()) { // 문제에 대한 예외처리
+        if (problems == null || problems.isEmpty()) { // TODO : 문제에 대한 예외처리
             //예외처리
         }
-        this.problems.addAll(problems);
+        // 문제를 가져와서 3개씩 나누어 담도록 함
+        // TODO : 3개가 아닌 여러개로 바꿀 수 있도록 수정
+        int numberOfRounds = (int) Math.ceil((double) problems.size() / 3); // 안 나눠떨어질 때 예외처리
+        for (int i = 0; i < numberOfRounds; i++) {
+            int startIndex = i * 3;
+            int endIndex = Math.min(startIndex + 3, problems.size());
+            roundProblems.add(problems.subList(startIndex, endIndex));
+        }
+
         this.isStart = true;
     }
 
