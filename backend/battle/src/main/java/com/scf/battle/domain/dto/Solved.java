315b98f759bdb2d09155f7a81997323a0ac8f430
박지훈 <zlxlgus123@naver.com>
2024-07-24 17:59:17 +0900

    faet #15 : Solved 생성

---
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/Solved.java
@@ -0,0 +1,15 @@
+package com.scf.battle.domain.dto;
+
+import java.util.Map;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Solved {
+
+    private final Long problemId;
+    private final Long userId;
+    private final Map<Integer, Integer> solve;
+    private final Integer submitTime;
+}
