45ee0504f2b890901c89e0d60055335de7b7b5f1
선예림 <syl1602@naver.com>
2024-07-29 14:43:59 +0900

    chore: #21. 코드 정렬

--- backend/user/src/main/java/com/scf/user/profile/domain/dto/HistoryResponseDto.java
+++ backend/user/src/main/java/com/scf/user/profile/domain/dto/HistoryResponseDto.java
@@ -1,10 +1,8 @@
 package com.scf.user.profile.domain.dto;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,7 +17,8 @@ public class HistoryResponseDto {
     private int gametype;
 
     // LocalDateTime을 String으로 변환하는 메서드
-    public static HistoryResponseDto from(LocalDateTime dateTime, int rank, int score, int gametype) {
+    public static HistoryResponseDto from(LocalDateTime dateTime, int rank, int score,
+        int gametype) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // 밀리초 제외
         String formattedTime = dateTime.format(formatter);
         return new HistoryResponseDto(formattedTime, rank, score, gametype);
