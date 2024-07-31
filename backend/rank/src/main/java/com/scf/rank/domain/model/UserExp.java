9d0dea1de93f859ed4d5722c4e4d55d31f7231da
김민욱 <kmw9093@gmail.com>
2024-07-31 21:18:26 +0900

    fix #27 : UserExp 수정

--- backend/rank/src/main/java/com/scf/rank/domain/model/UserExp.java
+++ backend/rank/src/main/java/com/scf/rank/domain/model/UserExp.java
@@ -1,11 +1,18 @@
 package com.scf.rank.domain.model;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
-@Builder
+@NoArgsConstructor
+@Getter
 public class UserExp {
+
+    @Builder
+    public UserExp(Long userId, Integer exp) {
+        this.userId = userId;
+        this.exp = exp;
+    }
 
     private Long userId;
     private int exp;
