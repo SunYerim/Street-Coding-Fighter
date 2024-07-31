cb88892dc760b6430072aa67e06bd511964cbf0b
박지훈 <zlxlgus123@naver.com>
2024-08-01 00:36:57 +0900

    Update SecurityConfig.java

--- backend/user/src/main/java/com/scf/user/member/global/config/SecurityConfig.java
+++ backend/user/src/main/java/com/scf/user/member/global/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 
                     CorsConfiguration configuration = new CorsConfiguration();
 
-                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                    configuration.setAllowedOrigins(Collections.singletonList("https://ssafy11s.com"));
                     configuration.setAllowedMethods(Collections.singletonList("*"));
                     configuration.setAllowCredentials(true);
                     configuration.setAllowedHeaders(Collections.singletonList("*"));
