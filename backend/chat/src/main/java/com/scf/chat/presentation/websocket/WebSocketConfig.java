55cd7a49bbd584e077cd3f26bebfe3bc50fcd3e3
박지훈 <zlxlgus123@naver.com>
2024-08-01 17:40:44 +0900

    fix #35 : CORS 추가

--- backend/chat/src/main/java/com/scf/chat/presentation/websocket/WebSocketConfig.java
+++ backend/chat/src/main/java/com/scf/chat/presentation/websocket/WebSocketConfig.java
@@ -12,12 +12,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/room"); // 메시지 브로커가 /topic으로 시작하는 주제를 구독하는 클라이언트에게 메시지 전송
-        config.setApplicationDestinationPrefixes("/send"); // 클라이언트에서 서버로 보낼 메시지의 prefix
+        // 메시지 브로커가 /room으로 시작하는 주제를 구독하는 클라이언트에게 메시지 전송
+        config.enableSimpleBroker("/room");
+        // 클라이언트에서 서버로 보낼 메시지의 prefix
+        config.setApplicationDestinationPrefixes("/send");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        // WebSocket 엔드포인트 등록 및 SockJS 지원
+        registry.addEndpoint("/ws-chat")
+            .setAllowedOriginPatterns("https://ssafy11s.com", "http://localhost:5173")
+            .withSockJS();
     }
 }
