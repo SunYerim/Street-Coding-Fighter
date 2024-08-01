eefcbf75ab26db3f1235a81eeda77ecb547007ae
김민욱 <kmw9093@gmail.com>
2024-08-02 00:34:28 +0900

    fix #38 : 직렬화/역직렬화 하기 위해 기본 생성자 생성

--- backend/multi/src/main/java/com/scf/multi/domain/dto/user/Rank.java
+++ backend/multi/src/main/java/com/scf/multi/domain/dto/user/Rank.java
@@ -2,12 +2,22 @@ package com.scf.multi.domain.dto.user;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder
+@NoArgsConstructor
 @Data
 public class Rank {
 
     private Long userId;
     private String username;
     private int score;
+    private int rank;
+
+    @Builder
+    public Rank(Long userId, String username, Integer score, Integer rank) {
+        this.userId = userId;
+        this.username = username;
+        this.score = score;
+        this.rank = rank;
+    }
 }
