38f3e587d70a690bc78c29b0394e1e54d0cc3a6b
박지훈 <zlxlgus123@naver.com>
2024-08-02 15:38:32 +0900

    Update SecurityConfig.java

--- backend/user/src/main/java/com/scf/user/member/global/config/SecurityConfig.java
+++ backend/user/src/main/java/com/scf/user/member/global/config/SecurityConfig.java
@@ -17,13 +17,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -46,43 +44,41 @@ public class SecurityConfig {
     }
 
     @Bean
-public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    LoginFilter loginFilter = new LoginFilter(authenticationManager(), jwtTokenProvider,
-        objectMapper(), redisService, memberDetailService, jwtCookieUtil);
-    loginFilter.setFilterProcessesUrl("/user/public/login"); // 로그인 엔드포인트 설정.
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        LoginFilter loginFilter = new LoginFilter(authenticationManager(), jwtTokenProvider,
+            objectMapper(), redisService, memberDetailService, jwtCookieUtil);
+        loginFilter.setFilterProcessesUrl("/user/public/login"); // 로그인 엔드포인트 설정.
 
-    httpSecurity
-        .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(Arrays.asList("https://ssafy11s.com", "http://localhost:5173"));
-                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                configuration.setAllowCredentials(true);
-                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-                configuration.setExposedHeaders(Arrays.asList("Authorization"));
-                configuration.setMaxAge(3600L);
-                return configuration;
-            }
-        }))
-        .and()
-        .csrf(AbstractHttpConfigurer::disable)
-        .formLogin(AbstractHttpConfigurer::disable)
-        .httpBasic(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/user/public/login", "/user/public/join",
-                "/user/public/validate/**", "/user/public/request-verification-code",
-                "/user/public/request-verification", "/user/public/change-password",
-                "/user/public/reissue")
-            .permitAll()
-            .anyRequest().authenticated())
-        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-            UsernamePasswordAuthenticationFilter.class)
-        .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
-        .sessionManagement(session -> session
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    return httpSecurity.build();
-}
-
-
+        httpSecurity
+            .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Arrays.asList("https://ssafy11s.com", "http://localhost:5173"));
+                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "text/plain"));
+                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                    configuration.setMaxAge(3600L);
+                    return configuration;
+                }
+            }))
+            .and()
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/user/public/login", "/user/public/join",
+                    "/user/public/validate/**", "/user/public/request-verification-code",
+                    "/user/public/request-verification", "/user/public/change-password",
+                    "/user/public/reissue")
+                .permitAll()
+                .anyRequest().authenticated())
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class)
+            .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class) // 로그인 필터 추가
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+    }
 }
