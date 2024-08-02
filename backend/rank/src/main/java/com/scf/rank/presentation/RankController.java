d78fe042f4cb9e6674f0d9df157a1be6168a4499
김민욱 <kmw9093@gmail.com>
2024-08-02 10:16:17 +0900

    feat #41 : Kafka Produce Config 및 로직 작성

--- backend/rank/src/main/java/com/scf/rank/presentation/RankController.java
+++ backend/rank/src/main/java/com/scf/rank/presentation/RankController.java
@@ -2,25 +2,27 @@ package com.scf.rank.presentation;
 
 import com.scf.rank.application.RankService;
 import com.scf.rank.domain.model.UserExp;
+import com.scf.rank.produceTest.UserExpProducer;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/rank")
 public class RankController {
 
     private final RankService rankService;
-
-    public RankController(RankService rankService) {
-        this.rankService = rankService;
-    }
+    private final UserExpProducer userExpProducer;
 
     @GetMapping("/total")
     public ResponseEntity<List<UserExp>> getTotalRankings() {
@@ -42,10 +44,17 @@ public class RankController {
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserExp> getRank(@PathVariable Long userId) {
-        UserExp userExp = rankService.getRank(userId);
+        UserExp userExp = rankService.getUserRank(userId);
         if (userExp == null) {
             throw new ResourceNotFoundException("유저에 해당되는 랭킹이 없습니다: " + userId);
         }
         return new ResponseEntity<>(userExp, HttpStatus.OK);
+    }
+
+    // 테스트용 produce 코드
+    @PostMapping("/produce")
+    public String produceUserExp(@RequestBody UserExp userExp) {
+        userExpProducer.sendUserExpMessage(userExp);
+        return "Message produced successfully";
     }
 }
