f811bb9f18cdda8032c927b8f4fb708c56ae7530
박지훈 <zlxlgus123@naver.com>
2024-08-04 23:26:31 +0900

    feat #47 : RoomCurrentRoundDTO 추가

---
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/Room/RoomCurrentRoundDTO.java
@@ -0,0 +1,10 @@
+package com.scf.battle.domain.dto.Room;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RoomCurrentRoundDTO {
+    private Integer CurrentRound;
+}
