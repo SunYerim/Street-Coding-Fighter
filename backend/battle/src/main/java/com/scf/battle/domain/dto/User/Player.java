73bb2e39c8be75ea79507a13015c2a7743063a19
박지훈 <zlxlgus123@naver.com>
2024-08-04 23:29:48 +0900

    feat #47 : Player에 solveds list 추가

--- backend/battle/src/main/java/com/scf/battle/domain/dto/User/Player.java
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/User/Player.java
@@ -1,8 +1,10 @@
 package com.scf.battle.domain.dto.User;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -11,4 +13,10 @@ public class Player {
     private Long userId;
     private String username;
     private Integer Hp;
+    private final List<Solved> solveds = new ArrayList<>();
+
+    public void addSolved(Solved solved) {
+        this.solveds.add(solved);
+    }
+
 }
