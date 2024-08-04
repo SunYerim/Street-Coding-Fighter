c19abd5dcd8aad6d60053f68f1b620c9477d3213
박지훈 <zlxlgus123@naver.com>
2024-08-04 23:28:03 +0900

    fix #47 : WebSocketConfig multi, chat과 구분을 위한 엔드포인트 수정

--- backend/battle/src/main/java/com/scf/battle/presentation/websocket/WebSocketConfig.java
+++ backend/battle/src/main/java/com/scf/battle/presentation/websocket/WebSocketConfig.java
@@ -11,15 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") //web socket connection이 최초로 이루어지는 곳(handshake)
-            .setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-battle") //web socket connection이 최초로 이루어지는 곳(handshake)
+                .setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // @Controller 객체의 @MessageMapping 메서드로 라우팅, 클라이언트가 서버로 메시지 보낼 URL 접두사(pub)
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.setApplicationDestinationPrefixes("/send");
         // /topic 로 클라이언트로 메시지 전달(sub)
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/room");
     }
 }
