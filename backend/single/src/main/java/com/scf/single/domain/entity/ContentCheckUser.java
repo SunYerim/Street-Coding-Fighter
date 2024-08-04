91a4131e64f3ed60290093d36e683c73a4f0684e
선예림 <syl1602@naver.com>
2024-08-04 14:00:13 +0900

    feat: #44. entity 작성

---
+++ backend/single/src/main/java/com/scf/single/domain/entity/ContentCheckUser.java
@@ -0,0 +1,40 @@
+package com.scf.single.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "content_check_user")
+public class ContentCheckUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "check_id")
+    private int checkId;
+
+    @Column(name = "complete")
+    private int complete;
+
+    @Column(name = "member_id")
+    private int memberId;
+
+    @Column(name = "content_id")
+    private int contentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id", nullable = false)
+    private Content content;
+
+
+}
