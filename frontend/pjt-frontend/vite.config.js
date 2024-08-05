358ad32e5a2b5a55e8e006116ab599110f01ca8a
dltkdgus482 <dltkdgus482@naver.com>
2024-08-05 17:02:35 +0900

    Fix: 소켓 라이브러리 문법 수정

--- frontend/pjt-frontend/vite.config.js
+++ frontend/pjt-frontend/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-  }
+  define: {
+    global: "globalThis", // 'globalThis'를 사용하여 브라우저 환경에서 'global'을 정의합니다.
+  },
 });
