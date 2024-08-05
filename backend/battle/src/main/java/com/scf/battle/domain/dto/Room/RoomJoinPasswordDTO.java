4483cf7a2b1942570a3ee7514f4ac574a667d389
박지훈 <zlxlgus123@naver.com>
2024-08-05 15:57:11 +0900

    fix

--- backend/battle/src/main/java/com/scf/battle/domain/dto/Room/RoomJoinPasswordDTO.java
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/Room/RoomJoinPasswordDTO.java
@@ -3,10 +3,12 @@ package com.scf.battle.domain.dto.Room;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RoomJoinPasswordDTO {
     private String password;
 
