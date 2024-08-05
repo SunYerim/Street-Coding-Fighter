daeb95338596f9cf67165b82df4e16c77890ffef
dltkdgus482 <dltkdgus482@naver.com>
2024-08-05 14:29:12 +0900

    Fix: 로컬 스토어 오류 수정

--- frontend/pjt-frontend/stores/LeaderboardStore.jsx
+++ frontend/pjt-frontend/stores/LeaderboardStore.jsx
@@ -1,42 +1,9 @@
 import { create } from "zustand";
 
 const testRanking = {
-  total: {
-    1: { name: "Bernie1", exp: 1000000 },
-    2: { name: "Bernie2", exp: 1000000 },
-    3: { name: "Bernie3", exp: 1000000 },
-    4: { name: "Bernie4", exp: 1000000 },
-    5: { name: "Bernie5", exp: 1000000 },
-    6: { name: "Bernie6", exp: 1000000 },
-    7: { name: "Bernie7", exp: 1000000 },
-    8: { name: "Bernie8", exp: 1000000 },
-    9: { name: "Bernie9", exp: 1000000 },
-    10: { name: "Bernie10", exp: 1000000 },
-  },
-  weekly: {
-    1: { name: "Bernie", exp: 1000000 },
-    2: { name: "Bernie", exp: 1000000 },
-    3: { name: "Bernie", exp: 1000000 },
-    4: { name: "Bernie", exp: 1000000 },
-    5: { name: "Bernie", exp: 1000000 },
-    6: { name: "Bernie", exp: 1000000 },
-    7: { name: "Bernie", exp: 1000000 },
-    8: { name: "Bernie", exp: 1000000 },
-    9: { name: "Bernie", exp: 1000000 },
-    10: { name: "Bernie", exp: 1000000 },
-  },
-  daily: {
-    1: { name: "Bernie", exp: 1000000 },
-    2: { name: "Bernie", exp: 1000000 },
-    3: { name: "Bernie", exp: 1000000 },
-    4: { name: "Bernie", exp: 1000000 },
-    5: { name: "Bernie", exp: 1000000 },
-    6: { name: "Bernie", exp: 1000000 },
-    7: { name: "Bernie", exp: 1000000 },
-    8: { name: "Bernie", exp: 1000000 },
-    9: { name: "Bernie", exp: 1000000 },
-    10: { name: "Bernie", exp: 1000000 },
-  },
+  total: [],
+  weekly: [],
+  daily: [],
 };
 
 const useLeaderboardStore = create((set) => ({
