99f6a91b0f52ff0a5cb9b3d8a43c4ec77e23670c
박지훈 <zlxlgus123@naver.com>
2024-08-06 12:27:38 +0900

    feat #59 : KafkaService 추가

---
+++ backend/battle/src/main/java/com/scf/battle/application/KafkaService.java
@@ -0,0 +1,63 @@
+package com.scf.battle.application;
+
+import com.scf.battle.domain.dto.Room.GameResultRoomDTO;
+import com.scf.battle.domain.dto.User.Player;
+import com.scf.battle.domain.dto.User.Solved;
+import com.scf.battle.domain.enums.GameResultType;
+import com.scf.battle.domain.enums.GameType;
+import com.scf.battle.domain.model.BattleGameRoom;
+import com.scf.battle.infrastructure.KafkaMessageProducer;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaService {
+
+    private final KafkaMessageProducer kafkaMessageProducer;
+
+    @Autowired
+    public KafkaService(KafkaMessageProducer kafkaMessageProducer) {
+        this.kafkaMessageProducer = kafkaMessageProducer;
+    }
+
+    public void sendToKafkaSolved(BattleGameRoom room) {
+        List<Player> players = Arrays.asList(room.getPlayerA(), room.getPlayerB());
+
+        players.forEach(player ->
+            Optional.ofNullable(player)
+                .map(Player::getSolveds)
+                .ifPresent(kafkaMessageProducer::sendSolved)
+        );
+    }
+
+    public void sendToKafkaGameResult(BattleGameRoom room, GameResultType determineValue) {
+        GameResultRoomDTO gameResultRoomDTO = new GameResultRoomDTO(
+            GameType.BATTLE,
+            LocalDateTime.now(),
+            room.getPlayerA().getUserId(),
+            room.getPlayerB().getUserId(),
+            determineValue
+        );
+        kafkaMessageProducer.sendGameResult(gameResultRoomDTO);
+    }
+
+    public void testSendToKafkaSolved(List<Solved> solved) {
+        kafkaMessageProducer.sendSolved(solved);
+    }
+
+
+    public void testSendToKafkaGameResult() {
+        GameResultRoomDTO gameResultRoomDTO = new GameResultRoomDTO(
+            GameType.BATTLE,
+            LocalDateTime.now(),
+            1L,
+            2L,
+            GameResultType.DRAW
+        );
+        kafkaMessageProducer.sendGameResult(gameResultRoomDTO);
+    }
+}
