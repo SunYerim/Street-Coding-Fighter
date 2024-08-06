9cab860b6963129f9011a99f58641520a331da69
박지훈 <zlxlgus123@naver.com>
2024-08-06 12:28:46 +0900

    refactor #59 : determineWinner 위치 수정

--- backend/battle/src/main/java/com/scf/battle/presentation/websocket/BattleSocketController.java
+++ backend/battle/src/main/java/com/scf/battle/presentation/websocket/BattleSocketController.java
@@ -1,26 +1,22 @@
 package com.scf.battle.presentation.websocket;
 
 import com.scf.battle.application.BattleGameService;
+import com.scf.battle.application.KafkaService;
 import com.scf.battle.domain.dto.Problem.ProblemResponse;
 import com.scf.battle.domain.dto.Problem.ProblemSelectDTO;
-import com.scf.battle.domain.dto.Room.JoinRoomUserDTO;
+import com.scf.battle.domain.dto.Room.JoinRoomSocketResponseDTO;
 import com.scf.battle.domain.dto.Room.ResultRoomDTO;
 import com.scf.battle.domain.dto.User.FightDTO;
-import com.scf.battle.domain.dto.Room.JoinRoomDTO;
-import com.scf.battle.domain.dto.Room.GameResultRoomDTO;
+import com.scf.battle.domain.dto.Room.JoinRoomRequestDTO;
 import com.scf.battle.domain.dto.User.Player;
 import com.scf.battle.domain.dto.User.Solved;
 import com.scf.battle.domain.enums.GameResultType;
-import com.scf.battle.domain.enums.GameType;
 import com.scf.battle.domain.model.BattleGameRoom;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import com.scf.battle.infrastructure.KafkaMessageProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -32,15 +28,16 @@ public class BattleSocketController {
 
     private final BattleGameService battleGameService;
     private final SimpMessagingTemplate messagingTemplate;
-    private final KafkaMessageProducer kafkaMessageProducer;
+
 
     @MessageMapping("/game/{roomId}/join")
-    public void joinRoom(@DestinationVariable String roomId, JoinRoomDTO joinRoomDTO) {
-        System.out.println(joinRoomDTO);
-        battleGameService.joinRoom(roomId, joinRoomDTO.getUserId(), joinRoomDTO.getUsername(), joinRoomDTO.getRoomPassword());
+    public void joinRoom(@DestinationVariable String roomId, JoinRoomRequestDTO joinRoomRequestDTO) {
+        System.out.println(joinRoomRequestDTO);
+        battleGameService.joinRoom(roomId, joinRoomRequestDTO.getUserId(), joinRoomRequestDTO.getUsername(), joinRoomRequestDTO.getRoomPassword());
         //BattleGameRoom room = battleGameService.findById(roomId); // 필요 없지만, 디버깅시 필요할 수 있음.
 
-        messagingTemplate.convertAndSend("/room/" + roomId, new JoinRoomUserDTO(joinRoomDTO.getUserId(), joinRoomDTO.getUsername())); // 실제로는 입장 메세지
+        messagingTemplate.convertAndSend("/room/" + roomId, new JoinRoomSocketResponseDTO(
+            joinRoomRequestDTO.getUserId(), joinRoomRequestDTO.getUsername())); // 실제로는 입장 메세지
     }
 
     @MessageMapping("/game/{roomId}/answer")
@@ -56,12 +53,12 @@ public class BattleSocketController {
 
         if (battleGameService.isRoundOver(roomId)) { // 라운드가 끝났다면
             if (room.getCurrentRound().equals(room.getFinalRound())) {
-                determineWinner(room);
+                battleGameService.determineWinner(room);
                 return;
             }
             // 큰 경우 예외처리
             if (room.getPlayerA().getHp() <= 0 || room.getPlayerB().getHp() <= 0) {
-                determineWinner(room);
+                battleGameService.determineWinner(room);
                 return;
             }
             battleGameService.sendRoundProblemToRoom(roomId); // 다시 초기 3문제 주기
@@ -102,57 +99,5 @@ public class BattleSocketController {
         // 상대방에게 선택된 문제 전송
         messagingTemplate.convertAndSend("/room/" + roomId + "/" + opponentUser.getUserId(), selectedProblem);
 
-    }
-
-    private void determineWinner(BattleGameRoom room) {
-
-        int playerAHp = room.getPlayerA().getHp();
-        int playerBHp = room.getPlayerB().getHp();
-
-        Long winnerId = null;
-        Long loserId = null;
-        GameResultType determineValue = GameResultType.DRAW;
-        if (playerAHp > playerBHp) {
-            determineValue = GameResultType.PLAYER_A_WINS;
-            winnerId = room.getPlayerA().getUserId();
-            loserId = room.getPlayerB().getUserId();
-        } else if (playerBHp > playerAHp) {
-            determineValue = GameResultType.PLAYER_B_WINS;
-            winnerId = room.getPlayerB().getUserId();
-            loserId = room.getPlayerA().getUserId();
-        } // 무승부는 null 값
-
-        Map<String, Long> result = Map.of(
-                "winner", winnerId != null ? winnerId : -1,
-                "loser", loserId != null ? loserId : -1
-        );
-        sendToKafkaSolved(room); // kafka에게 유저별 문제 풀이 보내기
-        sendToKafkaGameResult(room, determineValue); // kafka에게 게임 결과 보내기
-        // 게임이 끝났으므로 클라이언트에게 알림
-        messagingTemplate.convertAndSend("/room/" + room.getRoomId(), new ResultRoomDTO(result));
-
-        // BattleGameRoom 삭제
-        battleGameService.removeRoom(room.getRoomId());
-    }
-
-    private void sendToKafkaSolved(BattleGameRoom room) {
-        List<Player> players = Arrays.asList(room.getPlayerA(), room.getPlayerB());
-
-        players.forEach(player ->
-                Optional.ofNullable(player)
-                        .map(Player::getSolveds)
-                        .ifPresent(kafkaMessageProducer::sendSolved)
-        );
-    }
-
-    private void sendToKafkaGameResult(BattleGameRoom room, GameResultType determineValue) {
-        GameResultRoomDTO gameResultRoomDTO = new GameResultRoomDTO(
-                GameType.BATTLE,
-                LocalDateTime.now(),
-                room.getPlayerA().getUserId(),
-                room.getPlayerB().getUserId(),
-                determineValue
-        );
-        kafkaMessageProducer.sendGameResult(gameResultRoomDTO);
     }
 }
