59115752036f9e8cdbfa1e7f7bebf10b9e67ef47
선예림 <syl1602@naver.com>
2024-08-06 12:08:30 +0900

    feat: Dockerfile 추가

--- backend/single/src/main/java/com/scf/single/presentation/SingleController.java
+++ backend/single/src/main/java/com/scf/single/presentation/SingleController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("edu")
+@RequestMapping("single")
 @Slf4j
 @RequiredArgsConstructor
 public class SingleController {
