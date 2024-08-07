7ba73bc5383b168d5939f3f7d2a6630fdcf43b60
정범수 <jbss0708@gmail.com>
2024-08-07 12:28:35 +0900

    Fix: 멀티 소켓 오류메세지 확인

--- frontend/pjt-frontend/src/components/game/server.js
+++ frontend/pjt-frontend/src/components/game/server.js
@@ -15,7 +15,7 @@ const newSocket = (roomId, memberId, name) => {
   const baseUrl = "wss://www.ssafy11s.com";
 
   const url = `${baseUrl}/ws-multi?roomId=${roomId}&userId=${memberId}&username=${name}`;
-  console.log(url);
+  console.log("Connecting to WebSocket URL:", url); 
   const socket = new WebSocket(url);
 
   socket.onopen = () => {
@@ -26,8 +26,8 @@ const newSocket = (roomId, memberId, name) => {
     console.log('WebSocket message received:', event.data);
   };
 
-  socket.onclose = () => {
-    console.log('WebSocket connection closed');
+  socket.onclose = (event) => {
+    console.log('WebSocket connection closed:', event);
   };
 
   socket.onerror = (error) => {
