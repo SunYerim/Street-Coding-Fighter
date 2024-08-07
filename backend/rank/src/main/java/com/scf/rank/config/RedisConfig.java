502165045d71ab51e1e042428e72fd46f8ca10c6
박지훈 <zlxlgus123@naver.com>
2024-08-07 12:13:25 +0900

    Update RedisConfig.java

--- backend/rank/src/main/java/com/scf/rank/config/RedisConfig.java
+++ backend/rank/src/main/java/com/scf/rank/config/RedisConfig.java
@@ -2,15 +2,30 @@ package com.scf.rank.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scf.rank.domain.model.UserExp;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    // Redis와 연결을 위한 Connection
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
 
     @Bean
     public RedisTemplate<String, UserExp> redisTemplate(RedisConnectionFactory connectionFactory) {
