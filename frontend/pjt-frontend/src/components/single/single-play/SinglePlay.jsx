3c7d9af78e47f40e79c3f36a89157a8284e6ead0
여대기 <daegi0923@naver.com>
2024-08-08 20:42:29 +0900

    Fix : property name 수정

--- frontend/pjt-frontend/src/components/single/single-play/SinglePlay.jsx
+++ frontend/pjt-frontend/src/components/single/single-play/SinglePlay.jsx
@@ -19,18 +19,186 @@ import { display } from '@mui/system';
 
 const testDialogueList = [
   {
-    page_no: 0,
-    script_content: '안녕! 나는 너희에게 프로그래밍을 \n 가르쳐줄 고양이 선생님이다냥. \n 아직 이름은 없다냥!',
-    action: 2,
-    imageCount: 0,
+      "pageNo": 0,
+      "scriptContent": "안녕! 나는 너희에게 프로그래밍을 \n 가르쳐줄 고양이 선생님이다냥. \n 아직 이름은 없다냥!",
+      "action": 0,
+      "imageCount": 0
   },
   {
-    page_no: 1,
-    script_content: '나는 프로그래밍의 기초부터 \n 하나씩 차근차근 알려줄 거다냥. \n 같이 열심히 공부하자냥!',
-    action: 1,
-    imageCount: 0,
+      "pageNo": 1,
+      "scriptContent": "나는 프로그래밍의 기초부터 \n 하나씩 차근차근 알려줄 거다냥. \n 같이 열심히 공부하자냥!",
+      "action": 0,
+      "imageCount": 0
   },
-];
+  {
+      "pageNo": 2,
+      "scriptContent": "오늘은 첫 시간으로 변수가 무엇인지 \n 알아볼 거다냥. 기대된다냥?",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 3,
+      "scriptContent": "프로그래밍이란 무엇일까냥? 프로그래밍은 \n 컴퓨터에게 명령을 내려서 일을 시키는 거다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 4,
+      "scriptContent": "그럼 변수란 무엇일까냥? \n 변수를 한 마디로  정의하자면,  \n 값을 저장하는 이름표라고 할 수 있다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 5,
+      "scriptContent": "변수는 메모리 공간에 있는 값에 이름을 \n 붙여주는 역할을 한다냥. \n 이렇게 하면 값을 쉽게 찾을 수 있다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 6,
+      "scriptContent": "변수는 어떠한 “한 값”을 가지고 있는 \n “메모리 공간 주소”에 붙인 “이름” \n 또는 메모리 공간 그 자체다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 7,
+      "scriptContent": "여기서 값이란, 메모리에 저장된 데이터를 \n 의미한다냥. 이 값은 숫자, 문자열, \n 리스트 등 여러 가지 형태로 존재할 수 있다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 8,
+      "scriptContent": "예를 들어, x = 5 라고 하면 \n x라는 이름의 변수에 5라는 값이 \n 저장되는 거다냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 9,
+      "scriptContent": "이때 = 기호는 할당 연산자라고 부르는데, \n 오른쪽의 값을 왼쪽의 변수에 \n 저장하는 역할을 한다냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 10,
+      "scriptContent": "그래서 x = 5는 5라는 값을 \n x라는 변수에 저장하라는 의미다냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 11,
+      "scriptContent": "변수의 값은 언제든지 바꿀 수 있다냥. \n 이번엔 x = 10으로 바꿔볼까냥?",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 12,
+      "scriptContent": "이렇게 하면 x는 이제 10이라는 \n 새로운 값을 가지게 된다냥. \n 간단하다냥!",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 13,
+      "scriptContent": "변수의 이름을 정할 때는 규칙이 있다냥. \n 알파벳, 숫자, 밑줄(_)만 사용할 수 있고, \n 숫자로 시작할 수 없다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 14,
+      "scriptContent": "예를 들어, 1cat은 안 되지만 \n cat1은 가능하다냥. \n 그리고 대소문자를 구분한다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 15,
+      "scriptContent": "변수명을 정할 때는 의미 있는 이름을 사용하는 것이 좋다냥. \n 예를 들어, numCats 같은 이름이 좋다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 16,
+      "scriptContent": "이제 변수를 이용해 값을 교환하는 방법을 알아보겠다냥. \n 예를 들어, x와 y의 값을 바꾸고 싶다면...",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 17,
+      "scriptContent": "temp = x \n x = y \n y = temp \n 이렇게 하면 x와 y의 값이 서로 바뀌게 된다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 18,
+      "scriptContent": "파이썬에서는 더 간단하게 \n x, y = y, x \n 이렇게 한 줄로도 값을 바꿀 수 있다냥!",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 19,
+      "scriptContent": "변수는 프로그램에서 데이터를 \n 저장하고 다루는 기본적인 도구다냥. \n 그래서 기초가 아주 중요하다냥!",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 20,
+      "scriptContent": "할당이란 값을 변수에 저장하는 \n 과정을 말한다냥. x = 5는 5라는 값을 \n x에 할당하는 것이다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 21,
+      "scriptContent": "값이란 변수에 저장된 데이터를 \n 의미한다냥. 숫자, 문자열, 리스트 등 \n 다양한 값이 있을 수 있다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 22,
+      "scriptContent": "평가란 표현식이나 문을 실행하여 \n 하나의 값을 얻는 과정을 말한다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 23,
+      "scriptContent": "예를 들어, x + 2는 x의 값에 \n 2를 더한 결과를 평가하여 \n 새로운 값을 얻는 것이다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 24,
+      "scriptContent": "변수를 왜 사용하는지에 대해 \n 설명할게다냥. 변수를 사용하면 \n 같은 값을 여러 번 재사용할 수 있다냥.",
+      "action": 0,
+      "imageCount": 0
+  },
+  {
+      "pageNo": 25,
+      "scriptContent": "예를 들어, print(1)을 사용해 \n 1을 세 번 출력한다고 생각해보자냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 26,
+      "scriptContent": "하지만, 2를 3번 출력한다고 바꾸면 \n print(1)에서 1을 세 번 바꿔야 한댜냥 \n 무한대로 늘렸을 때는? 너무 귀찮다냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 27,
+      "scriptContent": "또한 특정 1만 2로 바꿔야 한다면? \n 어떤 기준으로 바꿔야 하는지 알 수 없다냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 28,
+      "scriptContent": "그래서 변수를 통한 데이터 관리는 \n 프로그래밍에서 필수적이다냥.",
+      "action": 0,
+      "imageCount": 1
+  },
+  {
+      "pageNo": 29,
+      "scriptContent": "다음 시간에는 자료형에 대해 \n 알아보겠다냥. 그럼 다음에 또 만나자냥!",
+      "action": 0,
+      "imageCount": 0
+  }
+]
 
 
 
@@ -296,7 +464,7 @@ export default function SinglePlay() {
                       color: 'black',
                       whiteSpace: 'pre-line',
                     }}
-                    sequence={[currentDialogue.script_content, 500]}
+                    sequence={[currentDialogue.scriptContent, 500]}
                     wrapper="p"
                     speed={50}
                   />
