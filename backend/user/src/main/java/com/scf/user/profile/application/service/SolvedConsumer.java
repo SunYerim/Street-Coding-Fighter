2d7149d322f48ad24f8c597261eecb1a225d25a3
선예림 <syl1602@naver.com>
2024-08-08 09:23:46 +0900

    fix: #69. SolvedConsumer - updateSolved 인자값 Solved의 List로 변경

--- backend/user/src/main/java/com/scf/user/profile/application/service/SolvedConsumer.java
+++ backend/user/src/main/java/com/scf/user/profile/application/service/SolvedConsumer.java
@@ -2,6 +2,7 @@ package com.scf.user.profile.application.service;
 
 import com.scf.user.profile.domain.dto.kafka.SolvedProblemKafkaRequestDto;
 import com.scf.user.profile.domain.dto.kafka.Solved;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,19 @@ public class SolvedConsumer {
 
     // 푼 문제를 받아옵니다.
     @KafkaListener(topics = "solved", containerFactory = "solvedKafkaListenerContainerFactory")
-    public void updateSolved(Solved solved) {
-        System.out.println("Solved Problem: " + solved);
-        SolvedProblemKafkaRequestDto solvedProblemKafkaRequestDto = new SolvedProblemKafkaRequestDto();
+    public void updateSolved(List<Solved> solvedList) {
+        // solvedList를 순회하면서 solved객체에 사용자가 푼 문제를 db에 저장합니다.
+        for (Solved solved : solvedList) {
+            SolvedProblemKafkaRequestDto solvedProblemKafkaRequestDto = new SolvedProblemKafkaRequestDto();
 
-        solvedProblemKafkaRequestDto.setCorrect(solved.getIsCorrect());
-        solvedProblemKafkaRequestDto.setChoice(solved.getSolveText()); // 주관식
-        solvedProblemKafkaRequestDto.setChoiceText(solved.getSolve()); // 빈칸, 객관식
+            solvedProblemKafkaRequestDto.setCorrect(solved.getIsCorrect());
+            solvedProblemKafkaRequestDto.setChoice(solved.getSolveText()); // 주관식
+            solvedProblemKafkaRequestDto.setChoiceText(solved.getSolve()); // 빈칸, 객관식
 
-        // 사용자가 푼 문제를 db에 저장시킵니다.
-        profileService.submitSolved(solved.getUserId(), solvedProblemKafkaRequestDto);
+            // 사용자가 푼 문제를 db에 저장시킵니다.
+            profileService.submitSolved(solved.getUserId(), solvedProblemKafkaRequestDto);
+        }
+
     }
 
 }
