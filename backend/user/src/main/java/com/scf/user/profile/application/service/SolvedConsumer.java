1379f3be0731defb15f9a736b061dcb9c268aacd
선예림 <syl1602@naver.com>
2024-08-08 09:43:44 +0900

    fix: #69. 변수명 수정

--- backend/user/src/main/java/com/scf/user/profile/application/service/SolvedConsumer.java
+++ backend/user/src/main/java/com/scf/user/profile/application/service/SolvedConsumer.java
@@ -16,17 +16,17 @@ public class SolvedConsumer {
 
     // 푼 문제를 받아옵니다.
     @KafkaListener(topics = "solved", containerFactory = "solvedKafkaListenerContainerFactory")
-    public void updateSolved(List<Solved> solvedList) {
+    public void updateSolved(List<Solved> solved) {
         // solvedList를 순회하면서 solved객체에 사용자가 푼 문제를 db에 저장합니다.
-        for (Solved solved : solvedList) {
+        for (Solved solve : solved) {
             SolvedProblemKafkaRequestDto solvedProblemKafkaRequestDto = new SolvedProblemKafkaRequestDto();
 
-            solvedProblemKafkaRequestDto.setCorrect(solved.getIsCorrect());
-            solvedProblemKafkaRequestDto.setChoice(solved.getSolveText()); // 주관식
-            solvedProblemKafkaRequestDto.setChoiceText(solved.getSolve()); // 빈칸, 객관식
+            solvedProblemKafkaRequestDto.setCorrect(solve.getIsCorrect());
+            solvedProblemKafkaRequestDto.setChoice(solve.getSolveText()); // 주관식
+            solvedProblemKafkaRequestDto.setChoiceText(solve.getSolve()); // 빈칸, 객관식
 
             // 사용자가 푼 문제를 db에 저장시킵니다.
-            profileService.submitSolved(solved.getUserId(), solvedProblemKafkaRequestDto);
+            profileService.submitSolved(solve.getUserId(), solvedProblemKafkaRequestDto);
         }
 
     }
