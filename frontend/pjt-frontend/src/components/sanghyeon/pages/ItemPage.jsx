ab34b9a3cc93fd156eb711b2189f4f41d81c4349
dltkdgus482 <dltkdgus482@naver.com>
2024-08-10 18:15:49 +0900

    Feat: 캐릭터, 의상 구매 기능 추가

--- frontend/pjt-frontend/src/components/sanghyeon/pages/ItemPage.jsx
+++ frontend/pjt-frontend/src/components/sanghyeon/pages/ItemPage.jsx
@@ -1,11 +1,38 @@
 import Header from "../components/Header";
 import "../../../css/ItemPage.css";
-import { useRef } from "react";
-
+import { useRef, useState } from "react";
+import createAuthClient from "../apis/createAuthClient";
+import Swal from "sweetalert2";
+import store from "../../../store/store.js";
 import pixelTicket from "/pixel-ticket.jpg";
 import movingGreenSlime from "../../../assets/characters/movingGreenSlime.gif";
+import renderCharacter from "../apis/renderCharacter.js";
 
 const ItemPage = () => {
+  const {
+    baseURL,
+    accessToken,
+    setAccessToken,
+    character,
+    setCharacter,
+    exp,
+    setExp,
+  } = store((state) => ({
+    baseURL: state.baseURL,
+    accessToken: state.accessToken,
+    setAccessToken: state.setAccessToken,
+    character: state.character,
+    setCharacter: state.setCharacter,
+    exp: state.exp,
+    setExp: state.setExp,
+  }));
+
+  const authClient = createAuthClient(
+    baseURL,
+    () => accessToken,
+    setAccessToken
+  );
+
   const cardRef = useRef(null);
   const overlayRef = useRef(null);
 
@@ -35,6 +62,99 @@ const ItemPage = () => {
     overlay.style.backgroundPosition = `100% 100%`; // 애니메이션과 함께 되돌리기
   };
 
+  const purchaseCharacterTicket = async () => {
+    if (exp < 500) {
+      Swal.fire({
+        icon: "error",
+        title: "포인트 부족",
+        text: "포인트가 부족합니다.",
+        timer: 3000,
+      });
+    }
+
+    try {
+      const purchaseRes = await authClient({
+        method: "GET",
+        url: "/user/gacha/character-type",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(purchaseRes.data);
+      // setCharacter(purchaseRes.data.type);
+      // setExp(exp - 500);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "아이템 구매 실패",
+        text: "아이템 구매에 실패하였습니다. 다시 시도해주세요.",
+        timer: 3000,
+      });
+    }
+  };
+
+  const purchaseClothesTicket = async () => {
+    if (exp < 500) {
+      Swal.fire({
+        icon: "error",
+        title: "포인트 부족",
+        text: "포인트가 부족합니다.",
+        timer: 3000,
+      });
+    }
+
+    try {
+      const purchaseRes = await authClient({
+        method: "GET",
+        url: "/user/gacha/character-cloth",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(purchaseRes.data);
+      // setCharacter(purchaseRes.data.type);
+      // setExp(exp - 500);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "아이템 구매 실패",
+        text: "아이템 구매에 실패하였습니다. 다시 시도해주세요.",
+        timer: 3000,
+      });
+    }
+  };
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slides = [
+    {
+      title: "알쏭달쏭 캐릭터 티켓",
+      description:
+        "사용하면 프로필의 캐릭터를 10가지 캐릭터와 일부 특별한 캐릭터 중 임의로 바꿔 주는 아이템입니다. 아이템을 사용하는 순간 프로필의 캐릭터가 바뀌며 영구적으로 적용되고, 현재 캐릭터는 사라집니다.",
+      imgSrc: pixelTicket,
+      buttonText: "구매 (500 P)",
+      onClick: () => purchaseCharacterTicket(), // 여기에 클릭 핸들러 추가
+    },
+    {
+      title: "알쏭달쏭 의상 티켓",
+      description:
+        "사용하면 프로필의 캐릭터의 의상을 10가지 의상과 일부 특별한 의상 중 임의로 바꿔 주는 아이템입니다. 아이템을 사용하는 순간 프로필의 캐릭터의 의상이 바뀌며 영구적으로 적용되고, 현재 의상은 사라집니다.",
+      imgSrc: pixelTicket,
+      buttonText: "구매 (500 P)",
+      onClick: () => purchaseClothesTicket(), // 여기에 클릭 핸들러 추가
+    },
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+    );
+  };
+
   return (
     <>
       <div className="item-outer-outer-container">
@@ -50,22 +170,44 @@ const ItemPage = () => {
                   <div className="item-point">포인트: 1,000,000 P</div>
                 </div>
                 <div className="item-info-container">
-                  <div className="item-info-upper-container">
-                    <div className="item-info-img-container">
-                      <img src={pixelTicket} alt="my-character" />
+                  <div className="carousel">
+                    <div
+                      className="carousel-inner"
+                      style={{
+                        transform: `translateX(-${currentSlide * 100}%)`,
+                      }}
+                    >
+                      {slides.map((slide, index) => (
+                        <div className="carousel-item" key={index}>
+                          <div className="item-info-upper-container">
+                            <div className="item-info-img-container">
+                              <img src={slide.imgSrc} alt="item" />
+                            </div>
+                            <div className="item-info-title-container">
+                              {slide.title}
+                            </div>
+                          </div>
+                          <div className="item-info">{slide.description}</div>
+                          <div className="item-info-button-container">
+                            <button onClick={slide.onClick}>
+                              {slide.buttonText}
+                            </button>
+                          </div>
+                        </div>
+                      ))}
                     </div>
-                    <div className="item-info-title-container">
-                      알쏭달쏭 캐릭터 티켓
-                    </div>
-                  </div>
-                  <div className="item-info">
-                    사용하면 프로필의 캐릭터를 10가지 캐릭터와 일부 특별한
-                    캐릭터 중 임의로 바꿔 주는 아이템입니다. 아이템을 사용하는
-                    순간 프로필의 캐릭터가 바뀌며 영구적으로 적용되고, 현재
-                    캐릭터는 사라집니다.
-                  </div>
-                  <div className="item-info-button-container">
-                    <button>구매 (1000 P)</button>
+                    <button
+                      className="carousel-control prev"
+                      onClick={prevSlide}
+                    >
+                      &#10094;
+                    </button>
+                    <button
+                      className="carousel-control next"
+                      onClick={nextSlide}
+                    >
+                      &#10095;
+                    </button>
                   </div>
                 </div>
               </div>
@@ -82,7 +224,7 @@ const ItemPage = () => {
                       <div className="item-character-rank">R</div>
                     </div>
                     <div className="item-character-img-container">
-                      <img src={movingGreenSlime} alt="my-character" />
+                      <img src={renderCharacter(0)} alt="my-character" />
                     </div>
                     <div className="item-character-name-container">
                       <div className="item-character-name">The Slime.</div>
