0d2089dacb76ba1f73fb157616be8a6dce67a20a
dltkdgus482 <dltkdgus482@naver.com>
2024-08-10 23:14:50 +0900

    Fix: 더미 데이터 삭제

--- frontend/pjt-frontend/src/components/sanghyeon/pages/BattleGamePage.jsx
+++ frontend/pjt-frontend/src/components/sanghyeon/pages/BattleGamePage.jsx
@@ -114,7 +114,7 @@ const BattleGamePage = () => {
   const [currentRound, setCurrentRound] = useState(0);
   const [EnemyProblems, setEnemyProblems] = useState([]); // 여기
   const [count, setCount] = useState(30);
-  const [gameStart, setGameStart] = useState(true); // 여기
+  const [gameStart, setGameStart] = useState(false); // 여기
   const [myProblem, setMyProblem] = useState({});
   const [selectMyProblem, setSelectMyProblem] = useState(false); // 상대가 내 문제를 선택했는지
   const [selectOpponentProblem, setSelectOpponentProblem] = useState(false); // 내가 상대방의 문제를 선택했는지
@@ -490,7 +490,7 @@ const BattleGamePage = () => {
     }
   }, [count]);
 
-  const [modalIsOpen, setModalIsOpen] = useState(true); // 여기
+  const [modalIsOpen, setModalIsOpen] = useState(false); // 여기
 
   const openModal = () => {
     setModalIsOpen(true);
