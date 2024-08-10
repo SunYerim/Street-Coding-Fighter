5bc04bdb26234ccb36bd9861ecb7b9339de8ebab
김민욱 <kmw9093@gmail.com>
2024-08-11 01:43:23 +0900

    fix #82 : multiGameService.afterConnectionClosed 메서드 수정 (마지막 플레이어가 나간 후 3초 후 재연결 되지 않으면 방 삭제)

--- backend/multi/src/main/java/com/scf/multi/presentation/websocket/MultiGameWebSocketHandler.java
+++ backend/multi/src/main/java/com/scf/multi/presentation/websocket/MultiGameWebSocketHandler.java
@@ -18,6 +18,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -33,6 +36,7 @@ public class MultiGameWebSocketHandler extends TextWebSocketHandler {
     private final MultiGameService multiGameService;
     private final static Map<String, String> rooms = new ConcurrentHashMap<>(); // session ID -> room ID (유저가 어떤 방에 연결됐는지 알려고)
     private final static Map<String, Set<WebSocketSession>> sessionRooms = new ConcurrentHashMap<>(); // room ID -> sessions (방에 연결된 유저들을 알려고)
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     @EventListener
     public void onGameStarted(GameStartedEvent event) throws Exception {
@@ -40,6 +44,17 @@ public class MultiGameWebSocketHandler extends TextWebSocketHandler {
         String roomId = event.getRoomId();
         List<ListDTO> problems = multiGameService.getProblems(roomId);
         broadcastMessageToRoom(roomId, "gameStart", problems);
+
+
+        // 마지막 유저가 방을 나간 후 3초 후에 방을 삭제하는 작업을 스케줄링
+        executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException();
+            }
+            checkAndDeleteRoom(roomId);
+        });
     }
 
     @Override
@@ -197,6 +212,13 @@ public class MultiGameWebSocketHandler extends TextWebSocketHandler {
                     multiGameService.finalizeGame(roomId);
                 }
             }
+        }
+    }
+
+    private void checkAndDeleteRoom(String roomId) {
+        Set<WebSocketSession> roomSessions = sessionRooms.get(roomId);
+        if (roomSessions == null || roomSessions.isEmpty()) {
+            multiGameService.deleteRoom(roomId);
         }
     }
 }
