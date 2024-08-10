858a25e39a187cf96f723bd0935635f2816048e1
박지훈 <zlxlgus123@naver.com>
2024-08-10 22:36:46 +0900

    fix #73 : items 리스트 수정

--- backend/battle/src/main/java/com/scf/battle/domain/dto/Problem/ItemList.java
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/Problem/ItemList.java
@@ -8,15 +8,10 @@ import java.util.List;
 
 public class ItemList {
     public static final List<Item> items = Arrays.asList(
-            new Item("혼란의 스크린", ItemType.ATTACK, new double[] {0.2, 0.2, 0.2, 0.1, 0.1}, Rarity.COMMON),
-            new Item("시간 단축", ItemType.ATTACK, new double[] {0.2, 0.2, 0.2, 0.1, 0.1}, Rarity.COMMON),
-            new Item("오답 유도", ItemType.ATTACK, new double[] {0.2, 0.2, 0.2, 0.2, 0.1}, Rarity.UNCOMMON),
-            new Item("문제 변경", ItemType.ATTACK, new double[] {0.2, 0.2, 0.2, 0.2, 0.1}, Rarity.UNCOMMON),
-            new Item("시간 경과 아이템", ItemType.ATTACK, new double[] {0.2, 0.2, 0.2, 0.2, 0.1}, Rarity.UNCOMMON),
-            new Item("시간 연장", ItemType.DEFENSE, new double[] {0.2, 0.2, 0.2, 0.1, 0.1}, Rarity.COMMON),
-            new Item("오답 제거", ItemType.DEFENSE, new double[] {0.2, 0.2, 0.2, 0.2, 0.1}, Rarity.UNCOMMON),
-            new Item("무적 시간", ItemType.SPECIAL, new double[] {0.0, 0.0, 0.1, 0.1, 0.2}, Rarity.RARE),
-            new Item("더블 포인트", ItemType.SPECIAL, new double[] {0.0, 0.0, 0.0, 0.1, 0.2}, Rarity.EPIC),
-            new Item("회복 아이템", ItemType.SPECIAL, new double[] {0.0, 0.0, 0.0, 0.1, 0.2}, Rarity.LEGENDARY)
+            new Item("체력 회복 아이템", ItemType.BUFF, new double[] {0.35, 0.3, 0.2, 0.1, 0.05}, Rarity.LEGENDARY),
+            new Item("시간 연장", ItemType.BUFF, new double[] {0.35, 0.3, 0.2, 0.1, 0.05}, Rarity.LEGENDARY),
+            new Item("혼란의 스크린", ItemType.DEBUFF, new double[] {0.05, 0.1, 0.2, 0.3, 0.35}, Rarity.COMMON),
+            new Item("시간 단축", ItemType.DEBUFF, new double[] {0.05, 0.1, 0.2, 0.3, 0.35}, Rarity.COMMON),
+            new Item("체력 감소 아이템", ItemType.DEBUFF, new double[] {0.05, 0.1, 0.2, 0.3, 0.35}, Rarity.COMMON)
     );
 }
