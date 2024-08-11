ef4f2b925f97d35baf62c30afaca0c8152f3caa5
정범수 <jbss0708@gmail.com>
2024-08-11 10:18:39 +0900

    Fix: 멀티헤더

--- frontend/pjt-frontend/src/components/game/MultiHeader.jsx
+++ frontend/pjt-frontend/src/components/game/MultiHeader.jsx
@@ -20,71 +20,38 @@ const MultiHeader = () => {
   const closeModal = () => {
     setModalIsOpen(false);
   };
-  // const { baseURL, accessToken, roomId, setNormalQuit } = store((state) => ({
-  //   baseURL: state.baseURL,
-  //   accessToken: state.accessToken,
-  //   roomId: state.roomId,
-  //   setNormalQuit: state.setNormalQuit,
-  // }));
-
-  // const quitMultiRoom = async () => {
-  //   try {
-  //     const checkQuit = confirm("방을 나가시겠습니까?");
-
-  //     if (!checkQuit) return;
-
-  //     Swal.fire({
-  //       text: "방을 나갔습니다.",
-  //       icon: "success",
-  //       timer: 3000,
-  //     });
-  //     navigate("/multi");
-  //   } catch (error) {
-  //     Swal.fire({
-  //       text: "방 나가기에 실패했습니다.",
-  //       icon: "error",
-  //       timer: 3000,
-  //     });
-  //     console.log(error);
-  //   }
-  // };
 
   return (
     <>
       <div className="header-container">
         <div
-          className="header-back-button"
+          className="header-title"
           onClick={() => {
-            navigate("/multi");
+            navigate('/');
+          }}
+        >
+          Street Coding Figther
+        </div>
+      </div>
+        <div className="header-back-button"  onClick={() => {
+            navigate("/main");
           }}
         >
           <MdOutlineKeyboardBackspace />
         </div>
-        <h2
-          className="header-title"
-          onClick={() => {
-            navigate("/main");
-          }}
-        >
-          Street Coding Figther
-        </h2>
-        <div className="header-icon">
-          <img
-            onClick={() => {
-              navigate("/profile");
-            }}
-            className="user-icon"
-            src={userIcon}
-            alt="memberIcon"
-          />
-          <img
-            onClick={openModal}
-            className="setting-icon"
-            src={settingIcon}
-            alt="settingIcon"
-          />
+        <div className='header-right'>
+          <div className="header-icon">
+            <img
+              onClick={() => {
+                navigate('/profile');
+              }}
+              className="user-icon"
+              src={userIcon}
+              alt="memberIcon"
+            />
+            <img onClick={openModal} className="setting-icon" src={settingIcon} alt="settingIcon" />
+          </div>
         </div>
-      </div>
       <Setting isOpen={modalIsOpen} onClose={closeModal} />
     </>
   );
