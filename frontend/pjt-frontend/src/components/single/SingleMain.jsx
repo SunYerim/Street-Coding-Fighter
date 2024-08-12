14556e36bbce30981b6e54132c7da4e4f1118257
여대기 <daegi0923@naver.com>
2024-08-12 16:31:57 +0900

    Fix : 변수 중복 에러 수정

--- frontend/pjt-frontend/src/components/single/SingleMain.jsx
+++ frontend/pjt-frontend/src/components/single/SingleMain.jsx
@@ -14,7 +14,6 @@ export default function SingleMain() {
   const { baseURL, accessToken } = store();
   const navigate = useNavigate();
   // const tempToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MCIsImF1dGgiOiJVU0VSIiwibWVtYmVySWQiOjkwLCJ1c2VybmFtZSI6IuqwgOyghOyCrOyXheu2gC3quYDrr7zsmrEiLCJpYXQiOjE3MjMwMTMzOTksImV4cCI6MTcyMzAxNjk5OX0.39EpgNaDW4Pwp1taoqcsgB74ORG4ZbIfc7mG9ZWfj0s'
-  const { baseURL, accessToken } = store();
   useEffect(() => {
     const getSingleInfo = () => {
       axios({
