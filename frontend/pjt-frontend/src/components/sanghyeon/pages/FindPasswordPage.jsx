1510e2cb972bb329a9ad4ebf6106385ec6b410d6
선예림 <syl1602@naver.com>
2024-08-12 20:04:03 +0900

    Update FindPasswordPage.jsx

--- frontend/pjt-frontend/src/components/sanghyeon/pages/FindPasswordPage.jsx
+++ frontend/pjt-frontend/src/components/sanghyeon/pages/FindPasswordPage.jsx
@@ -20,6 +20,8 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import InputAdornment from "@mui/material/InputAdornment";
+
 
 
 const FindPasswordPage = () => {
