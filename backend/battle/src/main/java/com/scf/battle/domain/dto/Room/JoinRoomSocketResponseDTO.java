cf6638243ca88ceb84c25cdd5300aaf4c83e72f4
박지훈 <zlxlgus123@naver.com>
2024-08-14 00:47:01 +0900

    fix #92: JoinRoomSocketResponseDTO UserCharaterTypeResponseDTO 필드 수정

--- backend/battle/src/main/java/com/scf/battle/domain/dto/Room/JoinRoomSocketResponseDTO.java
+++ backend/battle/src/main/java/com/scf/battle/domain/dto/Room/JoinRoomSocketResponseDTO.java
@@ -1,5 +1,6 @@
 package com.scf.battle.domain.dto.Room;
 
+import com.scf.battle.domain.dto.User.UserCharaterTypeResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,5 @@ import lombok.NoArgsConstructor;
 public class JoinRoomSocketResponseDTO {
     private Long userId;
     private String username;
-    private Integer guestCharacterType;
+    private UserCharaterTypeResponseDTO guestCharacter;
 }
