e871c4fa1e88d733628b2663d21e08dce62c7bbb
dltkdgus482 <dltkdgus482@naver.com>
2024-08-14 12:40:49 +0900

    Design: 빈칸 채우기 border 삭제

--- frontend/pjt-frontend/src/components/game/quiz_with_blank/ChoiceContainer.jsx
+++ frontend/pjt-frontend/src/components/game/quiz_with_blank/ChoiceContainer.jsx
@@ -1,14 +1,14 @@
 import { border } from "@mui/system";
 
 const style = {
-  display: 'flex',
-  flexWrap: 'wrap',
-  padding: '10px',
-  height: 'auto',
-  maxWidth: '100%',
+  display: "flex",
+  flexWrap: "wrap",
+  padding: "10px",
+  height: "auto",
+  maxWidth: "100%",
   // backgroundColor: '#007BFF',
-  border: '2px solid #8216f5',
-  borderRadius: '5px',
+  // border: '2px solid #8216f5',
+  borderRadius: "5px",
 };
 
 const ChoiceContainer = ({ children }) => {
