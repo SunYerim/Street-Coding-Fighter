3d2a4a3a87e2eaeba406a7d58923b740e1072b2d
dltkdgus482 <dltkdgus482@naver.com>
2024-08-14 10:32:27 +0900

    Design: 배틀, 빈칸 채우기 문제 디자인 수정

--- frontend/pjt-frontend/src/components/game/quiz_with_blank/ChoiceContainer.jsx
+++ frontend/pjt-frontend/src/components/game/quiz_with_blank/ChoiceContainer.jsx
@@ -1,8 +1,16 @@
 const style = {
-  marginTop: "20px",
-  padding: "10px",
-  height: "50px",
-  backgroundColor: "#007BFF",
+  gap: "10px",
+  margin: "0 auto",
+  display: "inline-block",
+  padding: "10px 10px",
+  fontFamily: "'Fira Code', Consolas, 'Courier New', Courier, monospace",
+  fontSize: "1.2rem",
+  fontWeight: "bold",
+  color: "white",
+  border: "1px solid white",
+  borderRadius: "5px",
+  letterSpacing: "1px",
+  textAlign: "center",
 };
 
 const ChoiceContainer = ({ children }) => {
