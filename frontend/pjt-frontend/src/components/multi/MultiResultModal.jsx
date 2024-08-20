26dd480e609f37219f138f25773c039fa24e1ac7
정범수 <jbss0708@gmail.com>
2024-08-20 11:33:04 +0900

    Feat: 멀티 모달 이펙트 추가

--- frontend/pjt-frontend/src/components/multi/MultiResultModal.jsx
+++ frontend/pjt-frontend/src/components/multi/MultiResultModal.jsx
@@ -1,21 +1,45 @@
 import React, { useRef, useEffect, useState } from "react";
 import MultiRanking from "./MultiRanking.jsx";
+import JSConfetti from 'js-confetti';
 
 
 const MultiResultModal = () => {
   
   const [modalOpen, setModalOpen] = useState(true);
-  const modalOutsisde = useRef();
+  const modalOutside = useRef();
+  const [jsConfetti, setJsConfetti] = useState(null);
+
+  useEffect(() => {
+      setJsConfetti(new JSConfetti());
+  }, []);
+
+  const handleClick = () => {
+    if (jsConfetti) {
+      jsConfetti.addConfetti({
+        confettiColors: [
+          "#CAB0FF"
+        ],
+        confettiNumber: 500,
+      });
+    }
+  };
+
+  const handleModalClick = (e) => {
+    if (e.target === modalOutside.current) {
+      setModalOpen(false);
+    }
+    handleClick(); // Trigger confetti when the modal is clicked
+  };
 
   return (
     <>
       {
         modalOpen &&
-        <div className={'result-modal-container'} ref={modalOutsisde} onClick={e => {
-          if (e.target === modalOutsisde.current) {
-            setModalOpen(false);
-          }
-        }}>
+        <div
+          className={'result-modal-container'}
+          ref={modalOutside}
+          onClick={handleModalClick}
+        >
           <div className="result-modal-content">
               <MultiRanking />
           </div>
