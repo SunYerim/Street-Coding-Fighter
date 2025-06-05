d992fdab8e8a9f805aee277b61149f06580abf6d
SunYerim <yaelim6@pukyong.ac.kr>
2025-06-05 15:05:22 +0900

    refactor #2: businessException클래스 재정의

--- backend/user/src/main/java/com/scf/user/member/global/error/exception/BusinessException.java
+++ backend/user/src/main/java/com/scf/user/member/global/error/exception/BusinessException.java
@@ -1,12 +1,25 @@
-package com.scf.user.member.global.exception;
+package com.scf.user.member.global.error.exception;
 
+import com.scf.user.member.global.error.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class BusinessException extends RuntimeException {
 
-    public BusinessException(String message) {
-        super(message);
+
+    private final String invalidValue;
+    private final String fieldName;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public BusinessException(String invalidValue, String fieldName, ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.invalidValue = invalidValue != null ? invalidValue.toString() : null;
+        this.fieldName = fieldName;
+        this.httpStatus = errorCode.getHttpStatus();
+        this.message = errorCode.getMessage();
     }
 
-    public BusinessException(String message, Throwable cause) {
-        super(message, cause);
-    }
+
 }
